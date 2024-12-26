36b9fb41adb21c17ee13effdd350844bd54df713
tmgast <tmgast@gmail.com>
2024-12-27 07:21:19 +0900

    update bundler. revise manifest structure.

--- bundle.sh
+++ bundle.sh
@@ -1,16 +1,16 @@
-bundle ()
-{
-    local dest dir
-    dest="bin"
-    dir="bin/touchview"
-    mkdir -p "$dir"
-    cp "__init__.py" "$dir"
-    cp "preferences.py" "$dir"
-    cp -r "source" "$dir"
-    cd "$dest"
-    zip -r "touchview.zip" "touchview"
-    cd ".."
-    rm -rf "$dir"
+bundle() {
+  local dest dir
+  dest="bin"
+  dir="bin/touchview"
+  mkdir -p "$dir"
+  cp "__init__.py" "$dir"
+  cp "preferences.py" "$dir"
+  cp "blender_manifest.toml" "$dir"
+  cp -r "source" "$dir"
+  cd "$dest"
+  zip -r "touchview.zip" "touchview"
+  cd ".."
+  rm -rf "$dir"
 }
 
 bundle
